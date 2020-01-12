1eeae3f2d1376993f3be84d591e35781f366e766
Florian Schaeffler <flo@schaeffler-online.com>
2020-01-12 03:04:49 +0200

    added stage variable check

--- README.md
+++ README.md
@@ -4,17 +4,28 @@
 
 ```
 const maintenanceMode = require('middy-maintenance-mode')
-middy(handler).use(maintenanceMode())
+middy(handler).use(maintenanceMode({
+	envVariable: 'MAINTENANCE_MODE',
+	envVariableValue: 'on',
+	isEnabled: () => {
+		const isActive = true
+		return isActive
+	},
+	message: 'service in maintainance mode'
+}))
 ```
 
 ## Options
 
 ```
-envVariable: 'MAINTENANCE_MODE', // env variable to check for
-envVariableValue: 'on', // value to check for maintainance mode
-inMaintainanceMode: undefined,
+envVariable: DEFAULT_VARIABLE, // env variable to check for
+envVariableValue: DEFAULT_VARIABLE_VALUE, // value to check for maintainance mode
+stageVariable: DEFAULT_VARIABLE,
+stageVariableValue: DEFAULT_VARIABLE_VALUE,
+isEnabled: undefined,
 statusCode: 503, // service unavailable
 headers: {
 	'Retry-After': 60 * 15 // 15 minutes
-}
+},
+message: undefined
 ```
--- index.js
+++ index.js
@@ -1,16 +1,26 @@
+const DEFAULT_VARIABLE = 'MAINTENANCE_MODE'
+const DEFAULT_VARIABLE_VALUE = 'on'
+
 const defaults = {
-	envVariable: 'MAINTENANCE_MODE', // env variable to check for
-	envVariableValue: 'on', // value to check for maintainance mode
-	inMaintainanceMode: undefined,
+	envVariable: DEFAULT_VARIABLE, // env variable to check for
+	envVariableValue: DEFAULT_VARIABLE_VALUE, // value to check for maintainance mode
+	stageVariable: DEFAULT_VARIABLE,
+	stageVariableValue: DEFAULT_VARIABLE_VALUE,
+	isEnabled: undefined,
 	statusCode: 503, // service unavailable
 	headers: {
 		'Retry-After': 60 * 15 // 15 minutes
-	}
+	},
+	message: undefined
 }
 
-const inMaintainanceMode = options => {
-	if (typeof options.inMaintainanceMode === 'function') {
-		return options.inMaintainanceMode()
+const isEnabled = (options, event) => {
+	if (typeof options.isEnabled === 'function') {
+		return options.isEnabled() === true
+	}
+
+	if (event && event[stageVariable] === options.stageVariableValue) {
+		return true
 	}
 
 	return process.env[options.envVariable] === options.envVariableValue
@@ -19,7 +29,7 @@ const inMaintainanceMode = options => {
 const checkForMaintainanceMode = (opts, handler, next) => {
 	const options = Object.assign({}, defaults, opts)
 
-	if (inMaintainanceMode(options)) {
+	if (isEnabled(options)) {
 		return handler.callback(null, {
 			statusCode: options.statusCode,
 			headers: options.headers,
